15c8f3ebc357c69da42562d73a88b204cf1348a2
keisuke-karino <keisuke.karino@linecorp.com>
2020-05-07 15:31:48 +0900

    Uodated daats

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <apexchart type="line" height="350" :options="chartOptions" :series="series"></apexchart>
+    <apexchart
+      type="area"
+      height="350"
+      :options="chartOptions"
+      :series="series"
+    ></apexchart>
   </div>
 </template>
 
 <script>
-import Apexchart from "vue-apexcharts"
+import Apexchart from "vue-apexcharts";
+
+const convertApexArrayToApexObject = nanosAndValuePair =>
+  nanosAndValuePair.reduce(
+    (apexObjs, [nanos, value]) => [...apexObjs, { x: nanos, y: value }],
+    []
+  );
 
 export default {
   name: "App",
@@ -19,19 +30,25 @@ export default {
         {
           name: "TEAM A",
           type: "area",
-          data: [44, 55]
+          data: convertApexArrayToApexObject([
+            [1588604400000, 0],
+            [1588690800000, 25]
+          ])
         },
         {
           name: "TEAM B",
           type: "column",
-          data: [55, 69]
+          data: convertApexArrayToApexObject([
+            [1588604400000, 0],
+            [1588690800000, 25]
+          ])
         }
       ],
       chartOptions: {
-        chart: {
-          height: 350,
-          type: "line"
-        },
+        // chart: {
+        //   height: 350,
+        //   type: "line"
+        // },
         stroke: {
           curve: "smooth"
         },
@@ -39,21 +56,37 @@ export default {
           type: "solid",
           opacity: [0.35, 1]
         },
-        labels: [
-          "Dec 01",
-          "Dec 02",
-          "Dec 03",
-          "Dec 04",
-          "Dec 05",
-          "Dec 06",
-          "Dec 07",
-          "Dec 08",
-          "Dec 09 ",
-          "Dec 10",
-          "Dec 11"
-        ],
+        // labels: [
+        //   "Dec 01",
+        //   "Dec 02",
+        //   "Dec 03",
+        //   "Dec 04",
+        //   "Dec 05",
+        //   "Dec 06",
+        //   "Dec 07",
+        //   "Dec 08",
+        //   "Dec 09 ",
+        //   "Dec 10",
+        //   "Dec 11"
+        // ],
         markers: {
           size: 0
+        },
+        // xaxis: {
+        //   type: "datetime",
+        //   labels: {
+        //     formatter(value, timestamp) {
+        //       return dateFormat({ seconds: 0, nanos: timestamp }, "YYYY.MM.DD");
+        //     }
+        //   }
+        // },
+        xaxis: {
+          type: "datetime"
+          // labels: {
+          //     formatter(value: string, timestamp: number) {
+          //         return dateFormat({seconds: 0, nanos: timestamp}, 'YYYY.MM.DD');
+          //     },
+          // },
         },
         yaxis: [
           {
